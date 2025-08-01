e60219a3ee6be2e2b9121890f4ae2dd46fcd687e
Aya Kamel <ayasaadtaha@gmail.com>
2025-08-01 14:14:09 +0200

    neue App.jsx added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Routes, Route} from "react-router-dom";
 import './App.css'
+import Header from "./components/Header.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 
 function App() {
 
-  return (
-    <>
-        console.log('Hello Webpost Blog!')
-    </>
-  )
+    return (
+        <>
+            <Routes>
+
+                <Route path="/"
+                       element={
+                           <>
+                               <Header/>
+                               <Home/>
+                           </>
+                       }
+                />
+                <Route path="/about"
+                       element={
+                           <>
+                               <Header/>
+                               <About/>
+                           </>
+                       }
+                />
+                <Route path="/contact"
+                       element={
+                           <>
+                               <Header/>
+                               <Contact/>
+                           </>
+                       }
+                />
+                <Route path="/login"
+                       element={
+                           <>
+                               <Header/>
+                               <Login/>
+                           </>
+                       }
+                />
+                <Route path="/register"
+                       element={
+                           <>
+                               <Header/>
+                               <Register/>
+                           </>
+                       }
+                />
+
+
+            </Routes>
+        </>
+    )
 }
 
 export default App
