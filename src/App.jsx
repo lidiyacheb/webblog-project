f5955ecc98b71a04afa3900753a9e57fa1b19aa7
lidiyacheb <lidia.saulite@gmail.com>
2025-08-01 14:03:17 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,61 @@
-
+import {Routes, Route} from "react-router-dom";
 import './App.css'
 import Header from "./components/Header.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 
 function App() {
 
   return (
-    <>
-        console.log('Hello Webpost Blog!')
-        <Header />
+      <>
+          <Routes>
+
+              <Route path="/"
+                     element={
+                         <>
+                             <Header/>
+                             <Home/>
+                         </>
+                     }
+              />
+              <Route path="/about"
+                     element={
+                         <>
+                             <Header/>
+                             <About/>
+                         </>
+                     }
+              />
+              <Route path="/contact"
+                     element={
+                         <>
+                             <Header/>
+                             <Contact/>
+                         </>
+                     }
+              />
+              <Route path="/login"
+                     element={
+                         <>
+                             <Header/>
+                             <Login/>
+                         </>
+                     }
+              />
+              <Route path="/register"
+                     element={
+                         <>
+                             <Header/>
+                             <Register/>
+                         </>
+                     }
+              />
+
+
+          </Routes>
     </>
   )
 }
